249aad51bab59e2b0a3d2978b4d09dfb7f8b546e
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2014-09-12 06:44:31 -0500

    Removed some print statements
    cleaned up variable names
    
    changed data structures to paired lists to get ordered plotting
    
    moved legend

--- extractUsage.py
+++ extractUsage.py
@@ -20,47 +20,40 @@ parser.add_argument('-s','--sum',help="Sum across pools",action='store_true')
 
 args=parser.parse_args()
 
-
 ### projects
 usage_projects=set(cut.get_db_projects(curs))
-print usage_projects
 if args.project:
     usage_projects=set(args.project).intersection(usage_projects)
 
-print usage_projects
-
 ### pools
-# get list of all pools
 usage_pools=cut.get_db_pools(curs)
-
-# replace list of pools with list from command-line, if present
 if args.pool:
     usage_pools=set(args.pool).intersection(usage_pools)
 
-# sum over all users of a given project for each pool
-sum_col_list = map(lambda x: "sum(" + x + ")", usage_pools)
-col_query = ','.join(sum_col_list)
+# sum over all users for each pool
+sum_usage_pools = map(lambda x: "sum(" + x + ")", usage_pools)
+col_query = ','.join(sum_usage_pools)
 
 # sum over all pools
 if args.sum:
-    col_query  += ',(' + '+'.join(sum_col_list) + ')'
+    col_query  += ',(' + '+'.join(sum_usage_pools) + ')'
 
 project_data = {}
-all_dates = set()
 
 for project in usage_projects:
     print project
     curs.execute('select enddate,' + col_query + ' from usage where userid in (select rowid from users where project=?) group by enddate', (project,))
-    project_data[project] = {}
+    project_data[project] = {'dates':[], 'usage':[]}
 
     rows = curs.fetchall()
 
     for row in rows:
-        project_data[project][datetime.datetime.strptime(row[0],'%Y-%m-%d')] = row[1:]
-    if (max(project_data[project].values()) > 0):
-        print max(project_data[project].values())
-        plt.plot_date(mpld.date2num(project_data[project].keys()),map(lambda x: x[-1], project_data[project].values()),"o",xdate=True,label=project)
+        project_data[project]['dates'].append(datetime.datetime.strptime(row[0],'%Y-%m-%d'))
+        project_data[project]['usage'].append(row[1:])
+    if (max(project_data[project]['usage']) > 0):
+        plt.plot_date(mpld.date2num(project_data[project]['dates']),map(lambda x: x[-1], project_data[project]['usage']),'-',xdate=True,label=project)
 
 #print project_data
-plt.legend()
+plt.legend(loc='upper left')
+plt.ylabel('cpu-hours per day')
 plt.show()
