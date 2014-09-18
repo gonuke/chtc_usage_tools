8442577df2358743960bc85ed69b74d42ddf8dc9
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2014-09-18 12:01:52 -0500

    Add ability to group by different time spans - default = month

--- extractUsage.py
+++ extractUsage.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser(description='A tool to extract usage data')
 parser.add_argument('--project',help='The name of a project over which to summarize the results',nargs="*",type=lambda s: unicode(s,'utf8'))
 parser.add_argument('--pool',help='Limit the data to a single pool',nargs="*")
 parser.add_argument('-s','--sum',help="Sum across pools",action='store_true')
+parser.add_argument('--span',choices=['day','month','year'],help="Time span across which to sum data",default='month')
 parser.add_argument('database',help='The name of a database file')
 
 args=parser.parse_args()
@@ -31,6 +32,13 @@ usage_pools=cut.get_db_pools(curs)
 if args.pool:
     usage_pools=set(args.pool).intersection(usage_pools)
 
+date_fmt_list= {'day':"%Y-%m-%d", 'month':"%Y-%m", 'year':"%Y"}
+sql_groupby_name = 'month'
+if args.span:
+    sql_groupby_name = args.span
+
+date_fmt = date_fmt_list[sql_groupby_name]
+
 # sum over all users for each pool
 sum_usage_pools = map(lambda x: "sum(" + x + ")", usage_pools)
 col_query = ','.join(sum_usage_pools)
@@ -45,19 +53,20 @@ fig = plt.figure()
 
 for project in usage_projects:
     print project
-    curs.execute('select enddate,' + col_query + ' from usage where userid in (select rowid from users where project=?) group by enddate', (project,))
+    sql_cmd = 'select strftime("' + date_fmt + '",enddate) as ' + sql_groupby_name + ',' + col_query + ' from usage where ' + 'userid in (select rowid from users where project=?) group by ' + sql_groupby_name 
+    curs.execute(sql_cmd, (project,))
     project_data[project] = {'dates':[], 'usage':[]}
 
     rows = curs.fetchall()
 
     for row in rows:
-        project_data[project]['dates'].append(datetime.datetime.strptime(row[0],'%Y-%m-%d'))
+        project_data[project]['dates'].append(datetime.datetime.strptime(row[0],date_fmt))
         project_data[project]['usage'].append(row[1:])
     if (max(project_data[project]['usage']) > 0):
         plt.plot_date(mpld.date2num(project_data[project]['dates']),map(lambda x: x[-1], project_data[project]['usage']),'-',xdate=True,label=project)
 
 #print project_data
 plt.legend(loc='upper left')
-plt.ylabel('cpu-hours per day')
+plt.ylabel('cpu-hours per ' + sql_groupby_name)
 fig.autofmt_xdate()
 plt.show()
